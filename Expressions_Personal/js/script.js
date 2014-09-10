4420070a8588096a26d278a9efa7ca4fbad9166a
SethReno4217 <reno.seth@yahoo.com>
2014-09-10 13:59:22 -0400

    Added var
    Added monthlycost variable

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -12,3 +12,5 @@ var sizeoftank;
 sizeoftank = 16;
 
 var mpg = Math.round(((daysperfillup/sizeoftank) * (dailymiles)) *100)/100;
+
+var monthlycost = Math.round((1/mpg) * costpergallon * (dailymiles * 30.417) * 100)/100;

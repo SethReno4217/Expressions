f290c46a1665df4f0bd8fc6ea94ed6f4d841d505
SethReno4217 <reno.seth@yahoo.com>
2014-09-10 14:00:19 -0400

    Added cosole.log
    Added console.log line

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -14,3 +14,5 @@ sizeoftank = 16;
 var mpg = Math.round(((daysperfillup/sizeoftank) * (dailymiles)) *100)/100;
 
 var monthlycost = Math.round((1/mpg) * costpergallon * (dailymiles * 30.417) * 100)/100;
+
+console.log(("Your monthly total cost for gas is $") + monthlycost);

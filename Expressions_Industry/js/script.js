7ff4f810fcd85d6e19283494326d12a3d789c4a9
SethReno4217 <reno.seth@yahoo.com>
2014-09-10 21:25:32 -0400

    Formatting

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,4 +1,5 @@
 //*Enter the number of degrees of the pitch angle
 var degrees;
-degrees = 30;
-console.log('pitch angle is ' + degrees + ' degrees.');
+    degrees = 30;
+        console.log('pitch angle is ' + degrees + ' degrees.');
+

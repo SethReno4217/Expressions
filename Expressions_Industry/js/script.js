1fe5586277db79845b3ecc45d6ea708e72e6fffd
SethReno4217 <reno.seth@yahoo.com>
2014-09-10 21:39:12 -0400

    Added variable and formatting

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -4,9 +4,12 @@ var degrees;
             console.log('pitch angle is ' + degrees + ' degrees.');
 
 //*Calculates the cosine of pitch angle of the roof, writes the cosine to the console to verify that it
-// was calculated correctly.
+// was calculated correctly. All that is needed to calculate this is the degrees of the pitch angle.
 var cosPitch;
         cosPitch = Math.cos(degrees * Math.PI / 180);
             console.log('pitch angle cosine is ' + cosPitch.toFixed(6));
 
 //*Input the width of your house
+var HouseWidth;
+        HouseWidth = 20
+            console.log('house width: ' + HouseWidth + ' feet.');

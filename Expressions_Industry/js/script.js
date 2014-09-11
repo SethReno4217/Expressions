a56f5b80586b8d6a7d5c79d00c0dc7057aecef0e
SethReno4217 <reno.seth@yahoo.com>
2014-09-10 21:36:50 -0400

    Added comment and formatting

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,6 +1,12 @@
 //*Enter the number of degrees of the pitch angle
 var degrees;
-    degrees = 30;
-        console.log('pitch angle is ' + degrees + ' degrees.');
+        degrees = 30;
+            console.log('pitch angle is ' + degrees + ' degrees.');
 
-//*Calculates the cosine of pitch angle of the roof
+//*Calculates the cosine of pitch angle of the roof, writes the cosine to the console to verify that it
+// was calculated correctly.
+var cosPitch;
+        cosPitch = Math.cos(degrees * Math.PI / 180);
+            console.log('pitch angle cosine is ' + cosPitch.toFixed(6));
+
+//*Input the width of your house

1e94fcfec7e9a3c2e0cbf23a6145ef52e13860a0
SethReno4217 <reno.seth@yahoo.com>
2014-09-20 12:17:20 -0400

    Added comment

--- Conditionals_Industry /js/industryscript.js
+++ Conditionals_Industry /js/industryscript.js
@@ -4,6 +4,13 @@ var bytevalue = prompt("Enter the amount of Bytes","1024") ;
 
 var selectunit = prompt("Enter the unit of measurement (Kb,Mb,Gb) ","Mb") ;
 
+//Validate that byte value is a number
+var verifyByte = parseFloat(bytevalue);
+
+
+//Validate that select unit is a string
+
+
 if (selectunit == "Bytes") {
     getresults = bytevalue; }
 

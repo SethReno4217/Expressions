0095ac8f54f3b1aa2afdc093cba76fae139924b1
SethReno4217 <reno.seth@yahoo.com>
2014-09-20 13:59:49 -0400

    Adding code

---
+++ Conditionals_Wacky /js/wackyscript.js
@@ -0,0 +1,5 @@
+var symptoms = ["Fever","Coughing","Boils","Loss of motor skills","Blood in urine","Weakness","Cold Sweat",
+    "Chest Pain","Dizziness","Rash","Tenderness","Soreness","Vomiting","Hair loss","Irritable Bowls","Joint Pain"];
+console.log("We have a list of " + symptoms.length + " symptoms." );
+
+Math.floor((Math.random() * 10) + 1);

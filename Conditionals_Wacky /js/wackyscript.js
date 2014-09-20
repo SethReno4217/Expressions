a4a7eb987adfca0eb5473698d657ee667818ea31
SethReno4217 <reno.seth@yahoo.com>
2014-09-20 14:58:31 -0400

    Adding code

--- Conditionals_Wacky /js/wackyscript.js
+++ Conditionals_Wacky /js/wackyscript.js
@@ -2,4 +2,13 @@ var symptoms = ["Fever","Coughing","Boils","Loss of motor skills","Blood in urin
     "Chest Pain","Dizziness","Rash","Tenderness","Soreness","Vomiting","Hair loss","Irritable Bowls","Joint Pain"];
 console.log("We have a list of " + symptoms.length + " symptoms." );
 
-Math.floor((Math.random() * 10) + 1);
+var age = prompt("Enter your Age", "23");
+var streetNumber = prompt("Enter your street number", "1024");
+
+
+//Math.floor((Math.random() * 10) + 1);
+var ageModulus = 1024 % 23;
+
+
+console.log("The age modulus of my street number is " + (ageModulus) );
+console.log("Therefore I have the following symptoms: " + symptoms[ageModulus] );

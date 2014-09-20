0242d1fa7f723b77684e8c291e91c949d9f7cee1
SethReno4217 <reno.seth@yahoo.com>
2014-09-20 11:57:01 -0400

    Added code

--- Conditionals_Industry /js/industryscript.js
+++ Conditionals_Industry /js/industryscript.js
@@ -2,3 +2,21 @@ var getresults;
 
 var bytevalue = 10000;
 
+var selectunit = "Gb";
+
+if (selectunit == "Bytes") {
+    getresults = bytevalue; }
+
+        else
+        if (selectunit == "Kb") {
+            getresults = bytevalue * 1024; }
+
+        else
+        if (selectunit == "Mb") {
+            getresults = bytevalue * 1024 * 1024; }
+
+        else
+        if (selectunit == "Gb") {
+            getresults = bytevalue * 1024 * 1024 * 1024; }
+
+console.log(getresults);

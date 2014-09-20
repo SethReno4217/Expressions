3218a64a8c9780c2208994e938e1f3987f21a70f
SethReno4217 <reno.seth@yahoo.com>
2014-09-20 15:41:01 -0400

    Added comments

--- Conditionals_Industry /js/industryscript.js
+++ Conditionals_Industry /js/industryscript.js
@@ -2,16 +2,22 @@ var getresults;
 
 var responseMessage = "";
 
+//Byte Value to be entered
 var bytevalue = prompt("Enter the amount of Bytes","1024") ;
 
+//Unit of measurement selected
 var selectunit = prompt("Enter the unit of measurement (Bytes,Kb,Mb,Gb) ","Mb") ;
 
+//One Byte = 1024
 var baseConstantByte = 1024;
 
+//Kb conversion
 var KbExponent = 1 ;
 
+//Mb Conversion
 var MbExponent = 2 ;
 
+//Gb Conversion
 var GbExponent = 3 ;
 
 //Validate that byte value is a number

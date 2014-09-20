e8d5eb5cddaad3fae3ffb7ae13b7ae21a88f4aa3
SethReno4217 <reno.seth@yahoo.com>
2014-09-20 00:18:04 -0400

    Adding code

--- Conditionals_Industry /js/industryscript.js
+++ Conditionals_Industry /js/industryscript.js
@@ -8,3 +8,12 @@ function compute() {
     //Calculates the pitch angle of your roof
     var degrees = getVal('pitchDegrees');
     log('pitch angle is ' + degrees + ' degrees.');
+
+    //Calculates the cosine of the pitch angle
+    if (hasValue(degrees)) {
+        var cosPitch = Math.cos(degrees * Math.PI / 180);
+        log('pitch angle cosine is ' + cosPitch.toFixed(6));
+
+        //Calculates the house width
+        var width = getVal('houseWidth');
+        log('house width: ' + width + ' feet.');

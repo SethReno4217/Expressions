03ed99a4ac65dcef9efc49a40a0014636b8bb651
SethReno4217 <reno.seth@yahoo.com>
2014-09-20 16:38:05 -0400

    Adding code

---
+++ Conditionals_Personal /js/personalscript.js
@@ -0,0 +1,10 @@
+
+var swordMaxDmg = 12;
+
+var axeMaxDmg = 8;
+
+var swordMdmg = Math.floor((Math.random() * swordMaxDmg));
+
+var axeMdmg = Math.floor((Math.random() * axeMaxDmg));
+
+console.log("Your Sword attack did " + swordMdmg + " damage.");

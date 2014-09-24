d1f52c1a1676ab5f8bd7b605df3afeffba6f4e05
SethReno4217 <reno.seth@yahoo.com>
2014-09-24 12:58:40 -0400

    Adding code

--- Functions_Personal/js/personalscript.js
+++ Functions_Personal/js/personalscript.js
@@ -5,3 +5,37 @@ function findFactors() {
     var value1 = 50;
 
     var value2 = 125;
+
+
+    if ( isNaN( value1 ) || isNaN( value2 ) ) {
+
+        alert( "Please enter valid number values" );
+
+        noCommonfactors.reset();
+
+        return;
+
+    }
+
+
+        if ( value1 == 0 || value2 == 0 ) {
+
+            alert( "No common factors" );
+
+            return;
+
+        }
+
+
+
+
+
+}
+
+}
+
+}
+console.log( "value1 = " + value1 + "\nvalue2 = " + value2 + "\nCommon Factors: " + answer );
+
+}
+console.log(findFactors());

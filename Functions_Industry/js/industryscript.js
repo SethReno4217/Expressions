d14c18395e97185af19b5503453decb9b7c941f8
SethReno4217 <reno.seth@yahoo.com>
2014-09-24 10:51:50 -0400

    Adding code

---
+++ Functions_Industry/js/industryscript.js
@@ -0,0 +1,34 @@
+function log(message) {
+    console.log(message.toLowerCase());
+}
+
+function compute() {
+
+    var degrees = getVal('pitchDegrees');
+    log('pitch angle is ' + degrees + ' degrees.');
+
+    if (hasValue(degrees)) {
+        var cosPitch = Math.cos(degrees * Math.PI / 180);
+        log('pitch angle cosine is ' + cosPitch.toFixed(6));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    }
+}
+}
+}
+}
+}
+}

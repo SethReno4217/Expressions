444fa0afc35a7fc058c2ef2ac354501a702e9d62
SethReno4217 <reno.seth@yahoo.com>
2014-09-25 13:14:36 -0400

    Formating

--- Functions_Wacky/js/wackyscript.js
+++ Functions_Wacky/js/wackyscript.js
@@ -1,6 +1,19 @@
-function rainbow(numOfSteps, step) {
-    var r, g, b;
-    var h = step / numOfSteps;
-    var i = ~~(h * 6);
-    var f = h * 6 - i;
-    var q = 1 - f;
+//Roll some Dice!
+//Wont make you better IRL
+
+//Random dice roll calculation
+function dice()
+{
+    return Math.floor(6*Math.random()+1);
+}
+
+//Displays your roll in an alert, refresh the page to roll again!
+function showDice()
+{
+    a=0;
+    b=0;
+    a=dice();
+    b=dice();
+    alert("You got "+a+" and "+b+" together that is "+(a+b));
+}
+console.log(showDice());

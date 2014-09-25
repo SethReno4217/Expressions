8415713d2a1a78efdf8201c9b9f2353b9385803a
SethReno4217 <reno.seth@yahoo.com>
2014-09-25 13:20:50 -0400

    Done

--- Functions_Wacky/js/wackyscript.js
+++ Functions_Wacky/js/wackyscript.js
@@ -2,9 +2,13 @@
 //Wont make you better IRL
 
 //Random dice roll calculation
+
+var diceSides = prompt("Enter how many sides the dice has" , "8");
+var forceofThrow = prompt("Enter how hard you want to throw the dice!" , "10");
+
 function dice()
 {
-    return Math.floor(6*Math.random()+1);
+    return Math.floor(diceSides*Math.random()+forceofThrow);
 }
 
 //Displays your roll in an alert, refresh the page to roll again!
